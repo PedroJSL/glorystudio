1cad4adcaa40e89edafd65c2da43f1a6897d7c39
PedroJSL <pedrojsl.dev@gmail.com>
2021-09-08 14:55:18 +0100

    Configuración inicial del proyecto con Vanilo y Vue

---
+++ config/concord.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'modules' => [
+        /**
+         * Example:
+         * VendorA\ModuleX\Providers\ModuleServiceProvider::class,
+         * VendorB\ModuleY\Providers\ModuleServiceProvider::class
+         *
+         */
+        Konekt\AppShell\Providers\ModuleServiceProvider::class => [
+            'ui' => [
+                'name' => 'Vanilo',
+                'url' => '/admin/product'
+            ]
+        ],
+        Vanilo\Framework\Providers\ModuleServiceProvider::class
+    ],
+    'register_route_models' => true
+];
